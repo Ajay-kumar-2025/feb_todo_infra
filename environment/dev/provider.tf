8acf9aea9c4c3f78857d27a1051daf698027543c
Ajay-kumar-2025 <ajay.kr931@gmail.com>
2026-02-15 02:21:41 +0530

    added backend block

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.59.0"
     }
   }
+  # backened block
+  backend "azurerm" {
+    resource_group_name  = "test-rg5678"
+    storage_account_name = "teststorage043187"
+    container_name       = "tf-container"
+    key                  = "tfstate"
+  }
 }
-
 provider "azurerm" {
-  # Configuration options
   features {}
   subscription_id = "7b0035a3-2bdd-440f-b694-5b328a80d3bd"
 }
